8ab6fddc724dce74b35631fc78b1adc884233e83
Potato-Y <projectpotatoy@gmail.com>
2022-06-23 10:20:51 +0900

    기본구성 추가

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,23 @@
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Header from './components/Header';
+import Home from './page/Home/Home';
+import Post from './page/Post/Post';
+import Setting from './page/Setting/Setting';
 
 function App() {
-  return <div>hi</div>;
+  return (
+    <Routes>
+      <Route path="/" element={<Header />}>
+        <Route index element={<Home />} />
+        <Route path="/settings" element={<Setting />} />
+        {/* 여기는 상세 게시글 */}
+        <Route path="/post" element={<Post />}>
+          <Route path=":id" element={<Post />} />
+        </Route>
+      </Route>
+    </Routes>
+  );
 }
 
 export default App;
