477d62f632e097359240e5f97be29915932e29ba
Potato-Y <projectpotatoy@gmail.com>
2022-07-11 00:38:20 +0900

    타이틀 수정
    메인 페이지에서 홈으로 가고자 할 시 새로고침 하도록 수정

--- frontend/src/components/Header/Header.js
+++ frontend/src/components/Header/Header.js
@@ -1,13 +1,26 @@
-import { NavLink, Outlet } from 'react-router-dom';
+import { NavLink, Outlet, useLocation, useNavigate } from 'react-router-dom';
 import './Header.css';
 
 const Header = () => {
+  let location = useLocation();
+  const navigate = useNavigate();
   return (
     <div>
       <div id="head-box">
-        <NavLink to="/" id="navlink-home" className={'navlink-to-reset'}>
-          Token-Briefing
-        </NavLink>
+        <span
+          id="navlink-home"
+          className="navlink-to-rest"
+          style={{ cursor: 'pointer' }}
+          onClick={() => {
+            if (location.pathname === '/') {
+              window.location.reload();
+            } else {
+              navigate('/');
+            }
+          }}
+        >
+          Token Briefing
+        </span>
         &nbsp;&nbsp;|&nbsp;&nbsp;
         <NavLink to="/settings" className={'navlink-to-reset'}>
           설정
