a62df5b4dc4a9e375c6aba0651de8193b1274463
Potato-Y <projectpotatoy@gmail.com>
2022-07-11 13:15:36 +0900

    1분마다 새로운 정보를 불러오는 기능 추가

--- frontend/src/components/Post/Memo/MemoPost.js
+++ frontend/src/components/Post/Memo/MemoPost.js
@@ -7,6 +7,7 @@ const MemoPost = (props) => {
   console.log('Load Memo Post');
   // 메모 포스트 리스트
   const [memoList, setMemoList] = useState(null);
+  let getMemoData = '';
 
   if (memoList === null) {
     // memoList가 null일 경우 메모의 정보를 가져오지 않은 것으로 인지, 데이터를 가져오기
@@ -14,9 +15,26 @@ const MemoPost = (props) => {
       if (response.data === '') {
         console.log('받은 최신 메모가 없습니다.');
       } else {
+        getMemoData = response.data;
         setMemoList(response.data.map((row) => <MemoItem key={row.key} mode="read" writer={row.writer} date={row.date} content={row.content} />));
       }
     });
+
+    // 새로 올라오는 메모 포스트를 읽어올 수 있도록 한다.
+    const reloadMomoPost = () =>
+      setTimeout(() => {
+        axios.get('/api/v1/memo/last10').then((response) => {
+          if (response.data === getMemoData) {
+            return;
+          } else {
+            getMemoData = response.data;
+            setMemoList(response.data.map((row) => <MemoItem key={row.key} mode="read" writer={row.writer} date={row.date} content={row.content} />));
+          }
+        });
+        reloadMomoPost();
+      }, 60000);
+
+    reloadMomoPost();
   }
 
   return (
--- frontend/src/components/Post/Toekn/TokenPost.js
+++ frontend/src/components/Post/Toekn/TokenPost.js
@@ -8,38 +8,60 @@ const TokenPost = (props) => {
   console.log('Load Token Post');
   // 토큰 포스트 최신 데이터
   const [tokenBriefing, setTokenBriefing] = useState(null);
+  let getTokenPostData;
+
+  const setTokenBriefingWidget = (response) => {
+    if (response.data.date === undefined) {
+      console.log('토큰 브리핑의 최신 데이터를 찾을 수 없음');
+      setTokenBriefing(<TokenBriefing mode={false} />);
+    } else {
+      // 컴퓨터의 표준 시간 상관 없이 항상 한국 시간이 나타나도록 하기
+      const localDate = new Date(); // locale 시간
+      // UTC 시간 계산
+      const utc = localDate.getTime() + localDate.getTimezoneOffset() * 60 * 1000;
+      // UTC -> KST (UTC + 9 시간)
+      const KR_TIME_EIFF = 9 * 60 * 60 * 1000;
+      const krDate = new Date(utc + KR_TIME_EIFF);
+      var nowTimeYear = krDate.getFullYear();
+      var nowTimeMonth = ('00' + (krDate.getMonth() + 1)).slice(-2);
+      var nowTimeDate = ('00' + krDate.getDate()).slice(-2);
+
+      // 현재 시간 (처리 시간)
+      const date = `${nowTimeYear}-${nowTimeMonth}-${nowTimeDate}`;
+
+      if (response.data.date.split(' ')[0] === date) {
+        // 만약 오늘 업로드 된 글이 맞다면 모드를 true
+        setTokenBriefing(<TokenBriefing mode={true} tokenData={response.data} />);
+      } else {
+        // 오늘 작성한 글이 아니라면 false
+        setTokenBriefing(<TokenBriefing mode={false} />);
+      }
+    }
+  };
 
   if (tokenBriefing === null) {
     // tokenData에 데이터가 없을 경우
     axios.get('/api/v1/tokenbriefing/last_latest_post').then((response) => {
-      if (response.data.date === undefined) {
-        console.log('토큰 브리핑의 최신 데이터를 찾을 수 없음');
-        setTokenBriefing(<TokenBriefing mode={false} />);
-      } else {
-        // 컴퓨터의 표준 시간 상관 없이 항상 한국 시간이 나타나도록 하기
-        const localDate = new Date(); // locale 시간
-        // UTC 시간 계산
-        const utc = localDate.getTime() + localDate.getTimezoneOffset() * 60 * 1000;
-        // UTC -> KST (UTC + 9 시간)
-        const KR_TIME_EIFF = 9 * 60 * 60 * 1000;
-        const krDate = new Date(utc + KR_TIME_EIFF);
-        var nowTimeYear = krDate.getFullYear();
-        var nowTimeMonth = ('00' + (krDate.getMonth() + 1)).slice(-2);
-        var nowTimeDate = ('00' + krDate.getDate()).slice(-2);
-
-        // 현재 시간 (처리 시간)
-        const date = `${nowTimeYear}-${nowTimeMonth}-${nowTimeDate}`;
-
-        if (response.data.date.split(' ')[0] === date) {
-          // 만약 오늘 업로드 된 글이 맞다면 모드를 true
-          setTokenBriefing(<TokenBriefing mode={true} tokenData={response.data} />);
-        } else {
-          // 오늘 작성한 글이 아니라면 false
-          setTokenBriefing(<TokenBriefing mode={false} />);
-        }
-      }
+      getTokenPostData = response.data;
+      setTokenBriefingWidget(response);
     });
   }
+
+  // 새로 올라오는 토큰 브리핑 포스트를 읽어올 수 있도록 한다.
+  const reloadMomoPost = () =>
+    setTimeout(() => {
+      axios.get('/api/v1/tokenbriefing/last_latest_post').then((response) => {
+        if (response.data === getTokenPostData) {
+          return;
+        } else {
+          getTokenPostData = response.data;
+          setTokenBriefingWidget(response);
+        }
+      });
+      reloadMomoPost();
+    }, 60000);
+
+  reloadMomoPost();
 
   return (
     <div className="token-wrap">
