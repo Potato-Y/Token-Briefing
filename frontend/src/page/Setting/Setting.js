89ff3d20a2c9e99f0041d1be42fd236b7f40db10
Potato-Y <projectpotatoy@gmail.com>
2022-07-20 00:56:43 +0900

    1.0.0
    불필요한 내용삭제
    - pug view
    - 시간 간격으로 정보를 다시 받아오는 코드
    
    사용된 오픈소스 내용 추가
    frontend 설정 페이지 하단에 해당 내용 추가

--- frontend/src/page/Setting/Setting.js
+++ frontend/src/page/Setting/Setting.js
@@ -6,6 +6,96 @@ import './Setting.css';
 function Setting() {
   useTitle('Token Briefing - 설정');
   const [writerList, setWriterList] = useState(null);
+  const usedOpenSource = [
+    {
+      name: 'axios',
+      license: 'MIT',
+      link: 'https://github.com/axios/axios/blob/v1.x/LICENSE',
+    },
+    {
+      name: 'react',
+      license: 'MIT',
+      link: 'https://github.com/facebook/react/blob/main/LICENSE',
+    },
+    {
+      name: 'react-dom',
+      license: 'MIT',
+      link: 'https://github.com/facebook/react/blob/main/LICENSE',
+    },
+    {
+      name: 'react-router',
+      license: 'MIT',
+      link: 'https://github.com/remix-run/react-router/blob/main/LICENSE.md',
+    },
+    {
+      name: 'react-scripts',
+      license: 'MIT',
+      link: 'https://github.com/facebook/create-react-app/blob/main/LICENSE',
+    },
+    {
+      name: 'web-vitals',
+      license: 'Apache-2.0',
+      link: 'https://github.com/GoogleChrome/web-vitals/blob/main/LICENSE',
+    },
+    {
+      name: '@testing-library/jest-dom',
+      license: 'MIT',
+      link: 'https://github.com/testing-library/jest-dom/blob/main/LICENSE',
+    },
+    {
+      name: '@testing-library/react',
+      license: 'MIT',
+      link: 'https://github.com/testing-library/react-testing-library/blob/main/LICENSE',
+    },
+    {
+      name: '@testing-library/user-event',
+      license: 'MIT',
+      link: 'https://github.com/testing-library/user-event/blob/main/LICENSE',
+    },
+    {
+      name: 'cookie-parser',
+      license: 'MIT',
+      link: 'https://github.com/expressjs/cookie-parser/blob/master/LICENSE',
+    },
+    {
+      name: 'debug',
+      license: 'MIT',
+      link: 'https://github.com/debug-js/debug/blob/master/LICENSE',
+    },
+    {
+      name: 'express',
+      license: 'MIT',
+      link: 'https://github.com/expressjs/express/blob/master/LICENSE',
+    },
+    {
+      name: 'http-errors',
+      license: 'MIT',
+      link: 'https://github.com/jshttp/http-errors/blob/master/LICENSE',
+    },
+    {
+      name: 'morgan',
+      license: 'MIT',
+      link: 'https://github.com/expressjs/morgan/blob/master/LICENSE',
+    },
+    {
+      name: 'sqlite3',
+      license: 'MIT',
+      link: 'https://github.com/TryGhost/node-sqlite3',
+    },
+  ].map((row) => (
+    <div>
+      <p>
+        -{row.name}
+        <br />
+        {row.license}
+        <br />
+        <a href={row.link} target="_blank" rel="noopener noreferrer">
+          {row.link}
+        </a>
+      </p>
+      <br />
+    </div>
+  ));
 
   // 작성자 리스트가 비어있으면 불러오기
   if (writerList === null) {
@@ -102,6 +192,12 @@ function Setting() {
           <input type="submit" value="저장" />
         </form>
       </p>
+      <br />
+      <br />
+      <div>
+        <p>사용된 오픈소스</p>
+        {usedOpenSource}
+      </div>
     </div>
   );
 }
